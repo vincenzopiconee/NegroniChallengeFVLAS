09a0d3763d2dcc3285fc02b7983cfcf070a7eb92
federicasgammato <fsgammato24@fed.idserve.net>
2024-11-04 17:18:49 +0100

    changed the text

--- NegroniChallengeFVLAS/ContentView.swift
+++ NegroniChallengeFVLAS/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Vincenzo break my balls!!")
+            Text("bellissima is the name of my branch")
         }
         .padding()
     }
