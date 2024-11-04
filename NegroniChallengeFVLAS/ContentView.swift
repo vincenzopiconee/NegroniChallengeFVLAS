d345af78e812bfb01ea9ac505334a9d7e0fc4104
vincenzopiconee <vincen.picone@studenti.unina.it>
2024-11-04 16:46:23 +0100

    Initial Commit

---
+++ NegroniChallengeFVLAS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NegroniChallengeFVLAS
+//
+//  Created by Vincenzo Picone on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NegroniChallengeFVLAS/NegroniChallengeFVLASApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegroniChallengeFVLASApp.swift
+//  NegroniChallengeFVLAS
+//
+//  Created by Vincenzo Picone on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NegroniChallengeFVLASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
