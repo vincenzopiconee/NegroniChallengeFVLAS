7cc4863399995a39d7990b2bdfcc5b52380492ef
LorenzoPizzuto <lorenzopizzuto05@gmail.com>
2024-11-04 17:13:20 +0100

    vstack

--- NegroniChallengeFVLAS/ContentView.swift
+++ NegroniChallengeFVLAS/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("new World !")
+            Text("Vincenzo break my balls!!")
         }
         .padding()
     }
