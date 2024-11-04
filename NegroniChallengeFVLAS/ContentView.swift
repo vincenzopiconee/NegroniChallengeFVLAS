6a0f6560d57fde5222341ae8eaaf2a12ee687b58
vincenzopiconee <vincen.picone@studenti.unina.it>
2024-11-04 17:18:26 +0100

    text in contentview

--- NegroniChallengeFVLAS/ContentView.swift
+++ NegroniChallengeFVLAS/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Vincenzo break my balls!!")
+            Text("my brach")
         }
         .padding()
     }
